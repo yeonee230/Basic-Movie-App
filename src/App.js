3c9b3e75e6094378e4e6fb7f55f66df2ef6ea2ac
SUYEON KIM <yeonee230@gmail.com>
2023-02-17 21:01:10 +0900

    practice : making Todo - set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Button from "./Button"; // 2. Component를 import한다. 
-import style from './App.module.css';
+
 
 function App() {
   return (
     <div>
-      <h1 className={style.title}>Hello</h1>
-      <Button text="NEXT"/>
+    
     </div>
   );
 }
