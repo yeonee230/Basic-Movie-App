ecc15a02cdec3652a95309dbb89b1a81aa5c1801
SUYEON KIM <yeonee230@gmail.com>
2023-02-19 22:11:44 +0900

    making movie app : css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres, year }) {
   return (
-    <div>
-      <img alt={title} src={coverImg} />
-      <h2><Link to={`movie/${id}`}>{title}</Link></h2>
-      <h3>{summary}</h3>
-      { genres ? (
-        <ul>
+    <div className={styles.movie}>
+      <img alt={title} src={coverImg} className={styles.movie__img} />
+      <h2 className={styles.movie__title}>
+        <Link to={`movie/${id}`}>{title}</Link>
+      </h2>
+      <h3 className={styles.movie__year}>{year}</h3>
+      <p>{summary.length > 230 ? `${summary.slice(0, 230)} ... ` : summary}</p>
+      {genres ? (
+        <ul className={styles.movie__genres}>
           {genres.map((genre) => (
             <li key={genre}>{genre}</li>
           ))}
@@ -18,12 +22,11 @@ function Movie({ id, coverImg, title, summary, genres }) {
   );
 }
 
-
-Movie.propTypes={
-    coverImg : PropTypes.string.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Movie;
--- src/routers/Home.js
+++ src/routers/Home.js
@@ -1,43 +1,46 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 const fetchUrl = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.0&sort_by=year`;
 
 function Home() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-      const response = await fetch(fetchUrl);
-      const json = await response.json();
-      setMovies(json.data.movies);
-      setLoading(false);
-    };
-  
-    useEffect(() => {
-      getMovies();
-    }, []); //api 실행
-  
-    return (
-      <div>
-        <h1>My Movie app</h1>
-        {loading ? (
-          <h1>Loading ... </h1>
-        ) : (
-          <div>
-            {movies.map((movie) => (
-              <Movie
-                id={movie.id}
-                key={movie.id}
-                coverImg={movie.medium_cover_image}
-                title={movie.title}
-                summary={movie.summary}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-    );
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(fetchUrl);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []); //api 실행
+
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div className={styles.movies}>
+          {movies.map((movie) => (
+            <Movie
+              id={movie.id}
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              year={movie.year}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
