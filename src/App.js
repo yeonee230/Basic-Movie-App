84487dd47552f5c6ef2e7b3bc5f63a97c530d3b6
SUYEON KIM <yeonee230@gmail.com>
2023-02-20 01:31:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-//import {  Routes, Route, HashRouter } from "react-router-dom";
+import {  Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./routers/Home";
 import Detail from "./routers/Detail";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/basic_movie">
       <Routes>
         <Route path={`/`} element={<Home />} />
         <Route path={`movie/:id`} element={<Detail />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
