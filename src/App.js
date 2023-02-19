4ac7b15624a1ce4572cb812350f204d2bc86f48f
SUYEON KIM <yeonee230@gmail.com>
2023-02-19 22:38:42 +0900

    deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
         <Route path="movie/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
