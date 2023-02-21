515f68401c6f0c47e9070d250c779a67c80b328f
SUYEON KIM <yeonee230@gmail.com>
2023-02-21 12:23:56 +0900

    1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
-        </HashRouter>
+        </BrowserRouter>
         );
